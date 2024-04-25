e162dddaca810024200c4103874842757ad92642
Sanskar <Sanskartayal2@gmail.com>
2024-04-25 19:18:31 +0530

    Implemented GetMapping ,handled Get Http requests and able to return Response objects in Json or XML

--- practice-app-ws/src/main/java/com/twpracticespring/appws/practiceappws/ui/controller/UserController.java
+++ practice-app-ws/src/main/java/com/twpracticespring/appws/practiceappws/ui/controller/UserController.java
@@ -1,17 +1,34 @@
 package com.twpracticespring.appws.practiceappws.ui.controller;
 
 
+import com.twpracticespring.appws.practiceappws.ui.model.respomse.UserRest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("users")
 public class UserController {
-    @GetMapping()
-    public String getUser()
-    {
-        return "Get User was called";
+    @GetMapping(path = "/{userid}", produces = {
+            MediaType.APPLICATION_JSON_VALUE,
+            MediaType.APPLICATION_XML_VALUE
+    })
+    public ResponseEntity<UserRest> getUser(@PathVariable String userid)
+     {
+         UserRest returnValue = new UserRest();
+         returnValue.setFirstName("Sanskar");
+         returnValue.setLastName("Tayal");
+         returnValue.setEmail("sanskar@tw.com");
+        return new ResponseEntity<>(returnValue, HttpStatus.OK) ;
     }
-
+    @GetMapping()
+    public String getUsers(@RequestParam(value = "page", defaultValue = "1", required = false)int page ,
+                           @RequestParam(value= "limit") int limit,
+                           @RequestParam(value= "sort", required = false, defaultValue = "desc") String sort)
+    {
+        return "Get Users was called with page no "+page+" and limit "+ limit+ " and sortvalue is " +sort;
+    }
     @PostMapping
     public String createUser()
     {
---
+++ practice-app-ws/src/main/java/com/twpracticespring/appws/practiceappws/ui/model/respomse/UserRest.java
@@ -0,0 +1,37 @@
+package com.twpracticespring.appws.practiceappws.ui.model.respomse;
+
+public class UserRest {
+    private String firstName, LastName, Email, userID;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+}
